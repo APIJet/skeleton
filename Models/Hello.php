72de97a28581095b1372b45b7711c13669a055be
GyunerZeki <me@gzeki.com>
2016-09-10 10:51:38 +0100

    fix typos, add db select example, better format of the code

--- Models/Hello.php
+++ Models/Hello.php
@@ -6,6 +6,9 @@ class Hello extends Base
 {
     public function getHelloMessage($name)
     {
+    	// exmaple of calling db
+        $this->getDb()->execQuery('SELECT 1');
+
         return "Hello ".$name.", I am PHP RESTFul API";
     }
 }
--- Public/index.php
+++ Public/index.php
@@ -22,31 +22,28 @@ $config->set([
             '{id}' => '([0-9]+)',
         ],
         'routes' => [
-            
             /**
-             * @desc Pattern for sending route
+             * @desc pattern for setting a route
              * '<url>' => ['<method_of_request>', '<Controller>\<Action>', '<LocalPatternNotReqired>'],
              */
             '' => [Router::GET, 'index\index'], // default router  
-            
+
             /**
-             * @desc route using local pattern
+             * @desc route using a local pattern
              * @example 
              *      of valid situation /hello_world/MyName, /hello_world/You, etc
-             *      of not valid /hello_world/5221, /hello_world/52f2add, etc
+             *      of invalid /hello_world/5221, /hello_world/52f2add, etc
              */
             'hello_world/{name}' => [Router::GET, 'hello\world', ['{name}' => '([a-zA-Z]+)']],
-            
-            
+
             /**
-             * @desc route overwriting global pattern path with local
+             * @desc route overwriting the global pattern path with a local one
              * @example 
-             *      of valid situation /hello_my_int/11111, /hello_world/643123, etc
-             *      of not valid /hello_world/7221132, hello_world/72211da32, etc
+             *      of valid situation /hello_my_int/123456, /hello_world/11111, etc
+             *      of invalid /hello_world/1234567, hello_world/AAA32, etc
              */
             'hello_my_int/{id}' => [Router::GET, 'hello\my_int', ['{id}' => '([1-6]+)']],
-            
-            
+
             /**
              * @desc match only specificy type of request
              * @example only POST and PUT 
@@ -59,9 +56,10 @@ $config->set([
 // load config file from external file
 $config->loadByJsonFile(ROOT_DIR.'config.json');
 
-// set all external singleton containers
+// will create instance immediately 
 $app->setSingletonContainer('Db', new Helpers\Db($config->get('Db')));
 
+// will create instance on demand
 $app->setSingletonContainer('Db2', function() use ($config) {
     return new Helpers\Db($config->get('Db'));
 });
